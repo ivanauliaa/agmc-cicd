b7c116cbdaf58380485047433dca6770cc81eb5b
Ivan Aulia Rahman <ivanar6542@gmail.com>
2022-09-24 16:45:17 +0700

    add say hello world test case

---
+++ internal/http/http_test.go
@@ -0,0 +1,49 @@
+package http_test
+
+import (
+	"day9-cicd/database/migration"
+	"day9-cicd/internal/factory"
+	server "day9-cicd/internal/http"
+	"day9-cicd/internal/middleware"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	os.Setenv("APP_ENV", "test")
+	godotenv.Load("../../.env")
+	migration.Migrate()
+}
+
+func TestHello_Hello(t *testing.T) {
+	f := factory.NewFactory()
+	e := echo.New()
+	middleware.Init(e)
+	server.NewHttp(e, f)
+
+	server := httptest.NewServer(e)
+	defer server.Close()
+
+	ex := httpexpect.WithConfig(httpexpect.Config{
+		BaseURL:  server.URL,
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
+
+	t.Run("should return 200", func(t *testing.T) {
+		resBody := ex.POST("/v1/hello").
+			Expect().
+			Status(http.StatusOK).
+			JSON().Object()
+
+		resBody.Value("message").Equal("hello world")
+	})
+}
